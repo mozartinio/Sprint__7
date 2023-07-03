63408c12612fa67d1633924e5414cdf6eff20c20
Maxim Parshin <mozartinio1998@gmail.com>
2023-07-03 13:45:23 +0300

    first commit after allure report

---
+++ src/main/java/ru/yandex/order/OrderInfo.java
@@ -0,0 +1,101 @@
+package ru.yandex.order;
+
+import java.util.List;
+
+public class OrderInfo {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String metroStation;
+    private String phone;
+    private String rentTime;
+    private String deliveryDate;
+    private String comment;
+    private List<String> colour;
+
+    public OrderInfo(List<String> colour) {
+        this.firstName = "Max";
+        this.lastName = "Maxov";
+        this.address = "Lazareva St., 60";
+        this.metroStation = "null Station";
+        this.phone = "+72222222";
+        this.rentTime = "1";
+        this.deliveryDate = "2023-05-23";
+        this.comment = "Whats up";
+        this.colour = colour;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMetroStation() {
+        return metroStation;
+    }
+
+    public void setMetroStation(String metroStation) {
+        this.metroStation = metroStation;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRentTime() {
+        return rentTime;
+    }
+
+    public void setRentTime(String rentTime) {
+        this.rentTime = rentTime;
+    }
+
+    public String getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(String deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public List<String> getColour() {
+        return colour;
+    }
+
+    public void setColour(List<String> colour) {
+        this.colour = colour;
+    }
+
+
+}
